8ac9348d18e71b27e9b5bfe6d75d3c680bd87e7c
Sushma Yadav <sushma.developer@appmanufact.com>
2022-11-23 22:55:16 +0530

    laravel first commit

---
+++ app/Models/Register.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Register extends Model
+{
+    use HasFactory;
+
+    protected $table = 'register';
+    protected $fillable = ['name','email','file_name'];
+}
